9d1e628f0a69c819cb89a2c34bea7eee822b1239
SelamwebEvangadi <selamwebbekele@gmail.com>
2024-12-17 14:39:07 -0500

    Signup page  added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import React, { createContext, useEffect } from "react"; // Add this line
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/HomePage/Home";
+import Register from "./pages/RegisterPage/Register";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
