a849fb93a647e5355213b98c0eb6eeba62e90edc
Bona Hirpa <bonaforg2@gmail.com>
2024-12-17 22:58:44 +0300

    Login page is added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/HomePage/Home";
 import Register from "./pages/RegisterPage/Register";
+import Login from "./pages/LoginPage/Login"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
